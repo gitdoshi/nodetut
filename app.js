9c142f4f63cecf90512d1ae61be25a58a6fa5881
Mohan Doshi <mohandoshi@Mohans-MacBook-Air-2.local>
2018-09-01 01:24:47 +0530

    Added comments

--- app.js
+++ app.js
@@ -2,5 +2,5 @@
 //console.log(addition.AddNumber(3,2));
 
 var localTutor = require('./NodeTutorial.js');
-var tut = new localTutor.NodeTutorial();
+var tut = new localTutor.NodeTutorial(); // use this way to make instances
 tut.pTutor();
